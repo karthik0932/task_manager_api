8bb133e8e96a76da142d3894543d85e88bd9f47a
Karthik S <karthiks9066@gmail.com>
2026-03-04 11:51:17 +0530

    Moved Django secret key to environment variable

--- task_manager_api/settings.py
+++ task_manager_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uz0u9470a%a&t_eu=9a$hqnwv@p^@xl^g%sthp%!j)6jkb$)q$'
+# SECRET_KEY = 'django-insecure-uz0u9470a%a&t_eu=9a$hqnwv@p^@xl^g%sthp%!j)6jkb$)q$'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "local-dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
